2a05f2b2af9ccfa12c92f32308e23b97800a8fed
Reyhan Marsalino Diansa <107743780+ReyhanDiansa@users.noreply.github.com>
2022-10-18 06:28:22 +0700

    Delete ukl_spp directory

--- ukl_spp/admin/search/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'project_ukl');
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
--- ukl_spp/admin/search/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-session_destroy();
-$_SESSION['status_login'] = false;
-header('location: ../../login/login.php');
-?>
